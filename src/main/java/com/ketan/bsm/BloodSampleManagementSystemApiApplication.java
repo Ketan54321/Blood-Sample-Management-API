9ca8eca59e0b816a14bbc74728714e1e34097c5a
Ketan54321 <ketanpavadshetty@email>
2025-01-21 19:16:51 +0530

    project-initiating

---
+++ src/main/java/com/ketan/bsm/BloodSampleManagementSystemApiApplication.java
@@ -0,0 +1,12 @@
+package com.ketan.bsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodSampleManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodSampleManagementSystemApiApplication.class, args);
+	}
+}
